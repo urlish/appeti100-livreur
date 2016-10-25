094eeac6cd130c4a20ddd5789e4fe23a09a6a046
urlish <ulrich@netsanslimites.com>
2016-10-25 23:36:01 +0200

    appeti100-livreur
    Version pour les livreurs

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={	
+	'ApiUrl':"http://www.appeti100.com/cg/driver/api",					
+	'DialogDefaultTitle':"Appeti100-Livreur",	
+	'PushProjectID':"651716383412",	
+	'APIHasKey':"2589cf428f3a451875dd8e7d3a5109a10"
+};
